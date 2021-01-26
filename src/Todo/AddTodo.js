0d766185e2adc1bdcc237ecab51e6fcd44de299e
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2021-01-26 18:23:12 +0500

    keep

--- src/Todo/AddTodo.js
+++ src/Todo/AddTodo.js
@@ -2,7 +2,16 @@ import { func } from 'prop-types'
 import React, { useState } from 'react'
 import Proptypes from 'prop-types'
 
+function useInputValue(defaultValue='') {
+    const [value, setValue] = useState(defaultValue)
+    return {
+        value,
+        onChange: event => setValue(event.target.value)
+    }
+}
+
 function AddTodo({ onCreate }) {
+    const input = useInputValue('')
     const [value, setValue] = useState('')
 
     function submitHandler(event) {
