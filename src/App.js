97015c4813d26552ee1446fb229a566ab9898270
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2021-01-26 18:52:19 +0500

    fetch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import TodoList from './Todo/TodoList'
 import Context from './context'
 import AddTodo from './Todo/AddTodo'
 function App() {
-  let [todos, setTodos] = React.useState([
-    {id: 1, completed: false, title: 'Купить 1231234'},
-    {id: 2, completed: false, title: 'Купить 452312'},
-    {id: 3, completed: false, title: 'Купить 732423'},
-  ])
+  let [todos, setTodos] = React.useState([])
   
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+        setTimeout(() => {
+          setTodos(todos)
+        }, 2020)
+      })
+  }, [])
+
   function toggleTodo(id) {
     setTodos(todos.map(todo => {
       if (todo.id === id) {
